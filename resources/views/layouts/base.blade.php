296e9385e1108a2bbe6ffd4d632b78daa579e78c
cumtnzg <cumtnzg@126.com>
2022-06-13 23:31:12 +0800

    layout base fixed

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -3,20 +3,13 @@
       dir="{{ config('app.rtl') ? 'rtl' : 'ltr' }}"
       class="@yield('body-class')">
 <head>
-    <title>{{ isset($pageTitle) ? $pageTitle . ' | ' : '' }}{{ setting('app-name') }}</title>
+    <title>学易 | 讲义</title>
 
-    <!-- Meta -->
     <meta name="viewport" content="width=device-width">
     <meta name="token" content="{{ csrf_token() }}">
     <meta name="base-url" content="{{ url('/') }}">
     <meta charset="utf-8">
 
-    <!-- Social Cards Meta -->
-    <meta property="og:title" content="{{ isset($pageTitle) ? $pageTitle . ' | ' : '' }}{{ setting('app-name') }}">
-    <meta property="og:url" content="{{ url()->current() }}">
-    @stack('social-meta')
-
-    <!-- Styles and Fonts -->
     <link rel="stylesheet" href="{{ versioned_asset('dist/styles.css') }}">
     <link rel="stylesheet" media="print" href="{{ versioned_asset('dist/print-styles.css') }}">
 
@@ -34,10 +27,48 @@
     @stack('translations')
 </head>
 <body class="@yield('body-class')">
+    <a class="px-m py-s skip-to-content-link print-hidden" href="#main-content">{{ trans('common.skip_to_main_content') }}</a>
 
-    @include('common.skip-to-content')
-    @include('common.notifications')
-    @include('common.header')
+    <header id="header" component="header-mobile-toggle" class="primary-background">
+        <div class="grid mx-l">
+
+            <div>
+                <a href="{{ url('/') }}" class="logo">
+                    @if(setting('app-logo', '') !== 'none')
+                        <img class="logo-image" src="{{ setting('app-logo', '') === '' ? url('/logo.png') : url(setting('app-logo', '')) }}" alt="Logo">
+                    @endif
+                    @if (setting('app-name-header'))
+                        <span class="logo-text">学易</span>
+                    @endif
+                </a>
+                <button type="button"
+                        refs="header-mobile-toggle@toggle"
+                        title="{{ trans('common.header_menu_expand') }}"
+                        aria-expanded="false"
+                        class="mobile-menu-toggle hide-over-l">@icon('more')</button>
+            </div>
+
+            <div class="flex-container-row justify-center hide-under-l">
+                <form action="{{ url('/search') }}" method="GET" class="search-box" role="search">
+                    <button id="header-search-box-button" type="submit" aria-label="{{ trans('common.search') }}" tabindex="-1">@icon('search') </button>
+                    <input id="header-search-box-input" type="text" name="term"
+                        aria-label="{{ trans('common.search') }}" placeholder="{{ trans('common.search') }}"
+                        value="{{ isset($searchTerm) ? $searchTerm : '' }}">
+                </form>
+            </div>
+
+            <div class="text-right">
+                <nav refs="header-mobile-toggle@menu" class="header-links">
+                    <div class="links text-center">
+                        <a class="hide-over-l" href="{{ url('/search') }}">@icon('search'){{ trans('common.search') }}</a>
+                        <a href="{{ url('/shelves') }}">@icon('bookshelf'){{ trans('entities.shelves') }}</a>
+                        <a href="{{ url('/books') }}">@icon('books'){{ trans('entities.books') }}</a>
+                    </div>                    
+                </nav>
+            </div>
+
+        </div>
+    </header>
 
     <div id="content" components="@yield('content-components')" class="block">
         @yield('content')
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -29,7 +29,46 @@
 <body class="@yield('body-class')">
     <a class="px-m py-s skip-to-content-link print-hidden" href="#main-content">{{ trans('common.skip_to_main_content') }}</a>
 
-    @include('common.header')
+    <header id="header" component="header-mobile-toggle" class="primary-background">
+        <div class="grid mx-l">
+
+            <div>
+                <a href="{{ url('/') }}" class="logo">
+                    @if(setting('app-logo', '') !== 'none')
+                        <img class="logo-image" src="{{ setting('app-logo', '') === '' ? url('/logo.png') : url(setting('app-logo', '')) }}" alt="Logo">
+                    @endif
+                    @if (setting('app-name-header'))
+                        <span class="logo-text">学易</span>
+                    @endif
+                </a>
+                <button type="button"
+                        refs="header-mobile-toggle@toggle"
+                        title="{{ trans('common.header_menu_expand') }}"
+                        aria-expanded="false"
+                        class="mobile-menu-toggle hide-over-l">@icon('more')</button>
+            </div>
+
+            <div class="flex-container-row justify-center hide-under-l">
+                <form action="{{ url('/search') }}" method="GET" class="search-box" role="search">
+                    <button id="header-search-box-button" type="submit" aria-label="{{ trans('common.search') }}" tabindex="-1">@icon('search') </button>
+                    <input id="header-search-box-input" type="text" name="term"
+                        aria-label="{{ trans('common.search') }}" placeholder="{{ trans('common.search') }}"
+                        value="{{ isset($searchTerm) ? $searchTerm : '' }}">
+                </form>
+            </div>
+
+            <div class="text-right">
+                <nav refs="header-mobile-toggle@menu" class="header-links">
+                    <div class="links text-center">
+                        <a class="hide-over-l" href="{{ url('/search') }}">@icon('search'){{ trans('common.search') }}</a>
+                        <a href="{{ url('/shelves') }}">@icon('bookshelf'){{ trans('entities.shelves') }}</a>
+                        <a href="{{ url('/books') }}">@icon('books'){{ trans('entities.books') }}</a>
+                    </div>                    
+                </nav>
+            </div>
+
+        </div>
+    </header>
 
     <div id="content" components="@yield('content-components')" class="block">
         @yield('content')
